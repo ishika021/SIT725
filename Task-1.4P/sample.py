234b90e5ffa267b966a17d737a6a314260abe2f0
ISHIKA RAJANI <ishikarajani21@gmail.com>
2025-03-05 13:44:36 +1100

    m

---
+++ Task-1.4P/sample.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+
+print("Hello, " + ' ' + name + ' ' + "Welcome!")
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult yet.")
+
+print("Have a great day!")

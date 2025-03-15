5d3c2c85ebb2af3df1f3c78bf76196d269f23a96
ISHIKA RAJANI <ishikarajani21@gmail.com>
2025-03-15 18:11:48 +1100

    m

--- Task-3.2P/server.js
+++ Task-3.2P/server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+const PORT = 4000;
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/register', (req, res) => {
+    console.log("Form Data Received:", req.body);
+    res.send("Registration Successful!");
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
